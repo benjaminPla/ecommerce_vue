fea25505f8afa73a524156deba1d84f1e6a8f8c5
benjaminPla <benjaminpla.dev@gmail.com>
2021-12-15 13:59:28 -0300

    add quantity

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,5 @@
 <template>
   <div class='card'>
-    <i @click.prevent='add' class="fas fa-cart-plus" :productId='info.id'></i>
     <img :src='info.image' :alt='info.title' />
     <div class='text-container'>
       <h3>{{ info.title }}</h3>
@@ -8,19 +7,25 @@
       <span class='price'>$ {{ info.price }}</span>
       <span class='rate'>&#9733; {{ info.rating.rate }}</span>
     </div>
+    <button @click.prevent='quantity > 0 && quantity --'>-</button>
+    <span>{{ quantity }}</span>
+    <button @click.prevent='quantity ++'>+</button>
+    <i @click.prevent='add' class="fas fa-cart-plus" :productId='info.id'></i>
   </div>
 </template>
 
 <script>
 import { useStore } from 'vuex';
+import { ref } from 'vue';
 
 export default {
   name: 'Product',
   props: ['info'],
   setup() {
     const store = useStore();
+    const quantity = ref(0);
     const add = (product) => store.commit('addCart', product.target.getAttribute('productId'));
-    return { add };
+    return { quantity, add };
   },
 };
 </script>
@@ -32,6 +37,7 @@ export default {
   background-color: #fff;
   border-radius: 10px;
   overflow: hidden;
+  padding-bottom: 20px;
   img {
     width: 100%;
     height: 250px;
